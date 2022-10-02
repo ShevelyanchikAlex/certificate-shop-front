1a7bfc9f24f672fd77fe8aaac517ac899447f09f
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-10-02 17:58:25 +0300

    added About Page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import CertificateShopLogo from "../../assets/images/shopping-cart.png";
+import '../../assets/styles/About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>Here will be data</p>
+        <div className={'about-container'}>
+            <div className={'about-header'}>
+                <img className="certificate-shop-logo"
+                     src={CertificateShopLogo}
+                     alt="Empty List Image"/>
+                <h1 className={'certificate-shop-name'}>Certificate Shop</h1>
+            </div>
+            <p className={'certificate-shop-description'}>Lots of Certificate ideas! Bright emotions for everyone from GiftCertificateShop. Huge catalog. Choose a
+                Certificate for yourself or as a gift! Any payment method. Fast and
+                convenient.</p>
         </div>
     );
 };

c0758ea865d8c5a5c3739b9a662918efe83bc66e
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-10-08 17:29:16 +0300

    implemented UserService, TagService, OrderService

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import {Cookies} from "react-cookie";
 
 const API_URL = "http://localhost:8080/auth";
+const cookies = new Cookies();
 
 class AuthService {
     static async login(request) {
@@ -10,6 +12,11 @@ class AuthService {
     static async signUp(request) {
         return await axios.post(`${API_URL}/signup`, request);
     };
+
+    static async logout() {
+        cookies.remove("token", {path: '/'});
+        localStorage.removeItem("user-email");
+    }
 }
 
 export default AuthService;
--- src/service/UserService.js
+++ src/service/UserService.js
@@ -5,9 +5,42 @@ const API_URL = "http://localhost:8080/users";
 const cookies = new Cookies();
 
 class UserService {
-
-    static async getAllUsers() {
+    static async getAllUsers(page = 1, size = 10) {
         return await axios.get(API_URL,
+            {
+                params: {
+                    pageIndex: page,
+                    size: size,
+                },
+                headers: {
+                    'Authorization': cookies.get("token"),
+                }
+            }
+        );
+    };
+
+    static async getUserById(id) {
+        return await axios.get(`${API_URL}/${id}`,
+            {
+                headers: {
+                    'Authorization': cookies.get("token"),
+                }
+            }
+        );
+    };
+
+    static async getUserByEmail(email) {
+        return await axios.get(`${API_URL}/email/${email}`,
+            {
+                headers: {
+                    'Authorization': cookies.get("token"),
+                }
+            }
+        );
+    };
+
+    static async getUserOrders(id) {
+        return await axios.get(`${API_URL}/${id}/orders`,
             {
                 headers: {
                     'Authorization': cookies.get("token"),

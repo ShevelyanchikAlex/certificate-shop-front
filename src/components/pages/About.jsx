b2abbfa33c6c3343e60fb1967d92dfb6b7108f7b
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-10-08 23:31:07 +0300

    changed logo and about icons

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../assets/styles/About.css';
-import AboutImage from "../../assets/images/logo.png";
+import AboutImage from "../../assets/images/about.png";
 
 const About = () => {
     return (
